55054e6c2d0a878bd1c4f37f27194e17d3329984
Alex Schmoe <aschmoe@gmail.com>
2014-10-29 18:09:25 -0700

    3rd scenario

--- js/services/useService.js
+++ js/services/useService.js
@@ -195,7 +195,7 @@ angular.module('healthLiteracy.use')
                         ]
                       },
                       showCosts: true,
-                      hint: "See what you’d pay if you went to your primary care provider"
+                      hint: ["See what you’d pay if you went to your primary care provider"]
                     },
                     {
                       text: 'The doctor/nurse practitioner sends you to get an MRI and blood test, for which you will owe more coinsurance.',
@@ -287,7 +287,7 @@ angular.module('healthLiteracy.use')
                         ]
                       },
                       showCosts: true,
-                      hint: "See what you’d pay if you went to your primary care provider"
+                      hint: ["See what you’d pay if you went to your primary care provider"]
                     },
                     {
                       text: 'The doctor/nurse practitioner sends you to get an MRI and blood test, for which you will owe more coinsurance.',
@@ -349,6 +349,464 @@ angular.module('healthLiteracy.use')
                   'results': {
                     'hint': 'If you’d gone to your Primary Care Provider?',
                     'text': 'You check out as healthy after the tests. You just had a bad virus, but your Primary Care Provider wants you to keep an eye on your cholesterol.'
+                  }
+                } 
+              ],
+              [// Nasty Cut
+                {// PRIMARY
+                  'stories': [
+                    {
+                      text: 'You’ve been paying your monthly premium for health insurance, so you have coverage when you need to be seen.',
+                      costs: {
+                        'insured': [
+                          {
+                            label: "Premium",
+                            group: 'premium',
+                            amount: 220,
+                            suffix: '/month'
+                          }
+                        ],
+                        'uninsured': [
+                          {
+                            group: "premium",
+                            amount: 0
+                          }
+                        ]
+                      },
+                      showCosts: false,
+                      hint: false
+                    },
+                    {
+                      text: 'Depending on how bad the cut is, your regular primary care provider may be able to stitch you up. Call them to find out.',
+                      costs: {
+                        'insured': [
+                          {
+                            label: "Copay",
+                            group: 'copay',
+                            amount: 25,
+                          },
+                          {
+                            label: "Cleaning wound/stitches",
+                            group: 'coinsurance',
+                            amount: 75,
+                          },
+                        ],
+                        'uninsured' : [
+                          {
+                            group: "visit",
+                            amount: 150
+                          },
+                          {
+                            group: 'stitches',
+                            amount: 150,
+                          },
+                        ]
+                      },
+                      showCosts: true,
+                      hint: false
+                    },
+                    {
+                      text: 'Unfortunately your cut starts looking a little like something from a horror film. It\'s getting infected. It happens, so don\'t freak out. Just get it taken care of! You go back to your provider to get it cleaned and restitched.',
+                      costs: {
+                        'insured': [
+                          {
+                            label: "Copay",
+                            group: 'copay',
+                            amount: 25,
+                          },
+                          {
+                            label: "Thorough cleaning/stitching",
+                            group: 'coinsurance',
+                            amount: 100,
+                          },
+                        ],
+                        'uninsured' : [
+                          {
+                            group: "visit",
+                            amount: 200
+                          },
+                          {
+                            group: 'stitches',
+                            amount: 350,
+                          },
+                        ]
+                      },
+                      showCosts: true,
+                      hint: false
+                    },
+                  ],
+                  'groups': {
+                    'premium': {
+                      'label': 'Premium',
+                      'weight': 0
+                    },
+                    'copay': {
+                      'label': 'Copays',
+                      'weight': 1
+                    },
+                    'coinsurance': {
+                      'label': 'Coinsurance',
+                      'weight': 2
+                    },
+                    'visit': {
+                      'label': 'Doctor visits',
+                      'weight': 1
+                    },
+                    'stitches': {
+                      'label': 'Cleanings/Stitches',
+                      'weight': 2
+                    }
+                  },
+                  'results': {
+                    'text': 'You check out as healthy after the tests. You just had a bad virus, but your Primary Care Provider wants you to keep an eye on your cholesterol.'
+                  }
+                },
+                {// URGENT
+                  'stories': [
+                    {
+                      text: 'You’ve been paying your monthly premium for health insurance, so you have coverage when you need to be seen.',
+                      costs: {
+                        'insured': [
+                          {
+                            label: "Premium",
+                            group: 'premium',
+                            amount: 220,
+                            suffix: '/month'
+                          }
+                        ],
+                        'uninsured': [
+                          {
+                            group: "premium",
+                            amount: 0
+                          }
+                        ]
+                      },
+                      showCosts: false,
+                      hint: false
+                    },
+                    {
+                      text: 'You head to Urgent Care to get stitched up.',
+                      costs: {
+                        'insured': [
+                          {
+                            label: "Copay",
+                            group: 'copay',
+                            amount: 75,
+                          },
+                          {
+                            label: "Coinsurance/Stitches",
+                            group: 'coinsurance',
+                            amount: 175,
+                          },
+                        ],
+                        'uninsured' : [
+                          {
+                            group: "visit",
+                            amount: 150
+                          },
+                          {
+                            group: 'stitches',
+                            amount: 400,
+                          },
+                        ]
+                      },
+                      showCosts: true,
+                      hint: false
+                    },
+                    {
+                      text: 'Unfortunately your cut starts looking a little like something from a horror film. It\'s getting infected. It happens, so don\'t freak out. Just get it taken care of! You go back to your provider to get it cleaned and restitched.',
+                      costs: {
+                        'insured': [
+                          {
+                            label: "Copay",
+                            group: 'copay',
+                            amount: 75,
+                          },
+                          {
+                            label: "Coinsurance/Stitches",
+                            group: 'coinsurance',
+                            amount: 200,
+                          },
+                        ],
+                        'uninsured' : [
+                          {
+                            group: "visit",
+                            amount: 150
+                          },
+                          {
+                            group: 'stitches',
+                            amount: 500,
+                          },
+                        ]
+                      },
+                      showCosts: true,
+                      hint: false
+                    },
+                  ],
+                  'groups': {
+                    'premium': {
+                      'label': 'Premium',
+                      'weight': 0
+                    },
+                    'copay': {
+                      'label': 'Copays',
+                      'weight': 1
+                    },
+                    'coinsurance': {
+                      'label': 'Coinsurance',
+                      'weight': 2
+                    },
+                    'visit': {
+                      'label': 'Doctor visits',
+                      'weight': 1
+                    },
+                    'stitches': {
+                      'label': 'Cleanings/Stitches',
+                      'weight': 2
+                    }
+                  },
+                  'results': {
+                    'text': 'You check out as healthy after the tests. You just had a bad virus, but your Primary Care Provider wants you to keep an eye on your cholesterol.'
+                  }
+                },
+                {//ER
+                  'stories': [
+                    {
+                      text: 'You’ve been paying your monthly premium for health insurance, so you have coverage when you need to be seen.',
+                      costs: {
+                        'insured': [
+                          {
+                            label: "Premium",
+                            group: 'premium',
+                            amount: 220,
+                            suffix: '/month'
+                          }
+                        ],
+                        'uninsured': [
+                          {
+                            group: "premium",
+                            amount: 0
+                          }
+                        ]
+                      },
+                      showCosts: false,
+                      hint: false
+                    },
+                    {
+                      text: 'You head to the Emergency Room (especially if you\'re bleeding badly) to get that gash taken care of.',
+                      costs: {
+                        'insured': [
+                          {
+                            label: "Copay",
+                            group: 'copay',
+                            amount: 150,
+                          },
+                          {
+                            label: "Coinsurance/Stitches",
+                            group: 'coinsurance',
+                            amount: 250,
+                          },
+                        ],
+                        'uninsured' : [
+                          {
+                            group: "visit",
+                            amount: 250
+                          },
+                          {
+                            group: 'stitches',
+                            amount: 800,
+                          },
+                        ]
+                      },
+                      showCosts: true,
+                      hint: false
+                    },
+                    {
+                      text: 'Unfortunately your cut starts looking a little like something from a horror film. It\'s getting infected. It happens, so don\'t freak out. Just get it taken care of! You go back to your provider to get it cleaned and restitched.',
+                      costs: {
+                        'insured': [
+                          {
+                            label: "Copay",
+                            group: 'copay',
+                            amount: 150,
+                          },
+                          {
+                            label: "Coinsurance/Stitches",
+                            group: 'coinsurance',
+                            amount: 350,
+                          },
+                        ],
+                        'uninsured' : [
+                          {
+                            group: "visit",
+                            amount: 250
+                          },
+                          {
+                            group: 'stitches',
+                            amount: 1000,
+                          },
+                        ]
+                      },
+                      showCosts: true,
+                      hint: false
+                    },
+                  ],
+                  'groups': {
+                    'premium': {
+                      'label': 'Premium',
+                      'weight': 0
+                    },
+                    'copay': {
+                      'label': 'Copays',
+                      'weight': 1
+                    },
+                    'coinsurance': {
+                      'label': 'Coinsurance',
+                      'weight': 2
+                    },
+                    'visit': {
+                      'label': 'Being seen in ER',
+                      'weight': 1
+                    },
+                    'stitches': {
+                      'label': 'Cleanings/Stitches',
+                      'weight': 2
+                    }
+                  },
+                  'results': {
+                    'text': 'After a few days to a week, your stitches are ready to come out and you\'re on the mend.'
+                  }
+                } 
+              ],
+              [// Appendicitis
+                {// PRIMARY
+                  'stories': [],
+                  'override': true,
+                  'results': {
+                    'text': 'Don\'t be stupid! That sharp, intense pain on the right side of your stomach could be serious. If you think you have appendicitis, GO TO THE EMERGENCY ROOM!!.'
+                  }
+                },
+                {// URGENT
+                  'stories': [],
+                  'override': true,
+                  'results': {
+                    'text': 'Don\'t be stupid! That sharp, intense pain on the right side of your stomach could be serious. If you think you have appendicitis, GO TO THE EMERGENCY ROOM!!.'
+                  }
+                },
+                {//ER
+                  'stories': [
+                    {
+                      text: 'You’ve been paying your monthly premium for health insurance, so you have coverage when you need to be seen.',
+                      costs: {
+                        'insured': [
+                          {
+                            label: "Premium",
+                            group: 'premium',
+                            amount: 220,
+                            suffix: '/month'
+                          },
+                          {
+                            label: "Pocket",
+                            group: 'outofpocket',
+                            amount: 4600
+                          },
+                        ],
+                        'uninsured': [
+                          {
+                            group: "premium",
+                            amount: 0
+                          }
+                        ]
+                      },
+                      showCosts: false,
+                      hint: false
+                    },
+                    {
+                      text: 'In the ER, a CT scan shows your appendix is ready to burst. You\'re admitted to the hospital.',
+                      costs: {
+                        'insured': [
+                          {
+                            label: "CT scan",
+                            group: 'scan',
+                            amount: 2000
+                          }
+                        ],
+                        'uninsured' : []
+                      },
+                      showCosts: true,
+                      hint: ["You just hit your health insurance deductible."]
+                    },
+                    {
+                      text: 'You have your appendix removed and you\'re in the hospital for 3 days to recover. Since you met your deductible, you pay 20% of remaining costs until you reach your out-of-pocket maximum.',
+                      costs: {
+                        'insured': [
+                          {
+                            label: "Overall Cost",
+                            group: 'overall',
+                            amount: 30000,
+                            skipCount: true
+                          },
+                        ],
+                        'uninsured' : [
+                          {
+                            group: "supplies",
+                            amount: 6823
+                          },
+                          {
+                            group: 'surgery',
+                            amount: 15168
+                          },
+                          {
+                            group: 'hospital',
+                            amount: 9000
+                          },
+                          {
+                            group: 'pharmacy',
+                            amount: 1000
+                          }
+                        ]
+                      },
+                      showCosts: true,
+                      hint: ['You pay $4,600 to reach your out-of-pocket maximum and your health insurance covers the rest!']
+                    },
+                  ],
+                  'groups': {
+                    'premium': {
+                      'label': 'Premium',
+                      'weight': 0
+                    },
+                    'scan': {
+                      'label': 'Deductible/CT scan',
+                      'weight': 1
+                    },
+                    'outofpocket': {
+                      'label': 'Costs to reach Out-of-pocket Max',
+                      'weight': 2,
+                    },
+                    'overall': {
+                      'label': 'Overall',
+                      'weight': 3,
+                      'hide': true
+                    },
+                    'supplies': {
+                      'label': 'Testing/supplies',
+                      'weight': 0
+                    },
+                    'surgery': {
+                      'label': 'Surgery',
+                      'weight': 1
+                    },
+                    'hospital': {
+                      'label': 'Hospital stay',
+                      'weight': 2
+                    },
+                    'pharmacy': {
+                      'label': 'Pharmacy costs',
+                      'weight': 3
+                    },
+                  },
+                  'results': {
+                    'text': 'That was a close call, but thankfully your insurance helped protect you from going into medical debt to pay for an emergency you had no control over.'
                   }
                 } 
               ]
