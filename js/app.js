1fc6e376a234038e5aad949c43f13b0a9d7ddfe2
Alex Schmoe <aschmoe@gmail.com>
2014-11-06 18:28:51 -0800

    adding phantomjs support

--- js/app.js
+++ js/app.js
@@ -28,6 +28,69 @@ angular.module('app', [
       $rootScope.$on('$stateChangeStart', 
         function(event, toState, toParams, fromState, fromParams){
 
+          // /* scrollTo -
+          // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+          // function scrollTo(eID) {
+     
+          //   // This scrolling function 
+          //   // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
+            
+          //   var i;
+          //   var startY = currentYPosition();
+          //   var stopY = elmYPosition(eID);
+          //   var distance = stopY > startY ? stopY - startY : startY - stopY;
+          //   if (distance < 100) {
+          //     scrollTo(0, stopY); return;
+          //   }
+          //   var speed = Math.round(distance / 100);
+          //   if (speed >= 20) speed = 20;
+          //   var step = Math.round(distance / 25);
+          //   var leapY = stopY > startY ? startY + step : startY - step;
+          //   var timer = 0;
+          //   if (stopY > startY) {
+          //     for (i = startY; i < stopY; i += step) {
+          //       setTimeout('window.scrollTo(0, '+leapY+')', timer * speed);
+          //       leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+          //     } return;
+          //   }
+          //   for (i = startY; i > stopY; i -= step) {
+          //     setTimeout('window.scrollTo(0, '+leapY+')', timer * speed);
+          //     leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+          //   }
+          // }
+          
+          // /* currentYPosition -
+          // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+          // function currentYPosition() {
+          //   // Firefox, Chrome, Opera, Safari
+          //   if (window.pageYOffset) {
+          //     return window.pageYOffset;
+          //   }
+          //   // Internet Explorer 6 - standards mode
+          //   if (document.documentElement && document.documentElement.scrollTop) {
+          //     return document.documentElement.scrollTop;
+          //   }
+          //   // Internet Explorer 6, 7 and 8
+          //   if (document.body.scrollTop) {
+          //     return document.body.scrollTop;
+          //   }
+          //   return 0;
+          // }
+     
+          // /* scrollTo -
+          // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+          // function elmYPosition(eID) {
+          //   var elm = document.getElementById(eID);
+          //   var y = elm.offsetTop;
+          //   var node = elm;
+          //   while (node.offsetParent && node.offsetParent != document.body) {
+          //     node = node.offsetParent;
+          //     y += node.offsetTop;
+          //   } return y;
+          // }
+
+          // scrollTo('#main');
+
           // If we have any data
           if(toState.data) {
             // Set title
@@ -57,10 +120,6 @@ angular.module('app', [
           metaInfo.resetAll();
         }
       );
-
-      $browser.notifyWhenNoOutstandingRequests(function () {
-        $rootScope.status = 'ready'; // or whatever
-      }
     }
 	]
 )
